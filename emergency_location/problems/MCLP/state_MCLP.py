43aab32d35dbae03cdb0af567f33ef14b2f7888a
Backlightever <624991421@qq.com>
2023-10-20 15:15:10 +0800

    add emergency location case

---
+++ emergency_location/problems/MCLP/state_MCLP.py
@@ -0,0 +1,149 @@
+import torch
+from typing import NamedTuple
+from utils.boolmask import mask_long2bool, mask_long_scatter
+
+
+def construct_solutions(actions):
+    return actions
+
+
+class StateMCLP(NamedTuple):
+    # Fixed input
+    loc: torch.Tensor
+    p: torch.Tensor
+    radius: torch.Tensor
+    dist: torch.Tensor
+
+    # If this state contains multiple copies (i.e. beam search) for the same instance, then for memory efficiency
+    # the loc and dist tensors are not kept multiple times, so we need to use the ids to   index the correct rows.
+    ids: torch.Tensor  # Keeps track of original fixed data index of rows
+
+    # State
+    first_a: torch.Tensor
+    prev_a: torch.Tensor
+    visited_: torch.Tensor  # Keeps track of nodes that have been visited
+    mask_cover: torch.Tensor
+    dynamic: torch.Tensor
+    facility: torch.Tensor  # B x p
+    cover_num: torch.Tensor
+    cur_coord: torch.Tensor
+    i: torch.Tensor  # Keeps track of step
+
+    @property
+    def visited(self):
+        if self.visited_.dtype == torch.bool:
+            return self.visited_
+        else:
+            return mask_long2bool(self.visited_, n=self.loc.size(-2))
+
+    @staticmethod
+    def initialize(data, visited_dtype=torch.bool):
+        loc = data['loc']
+        p = data['p'][0]
+        radius = data['r'][0]
+        batch_size, n_loc, _ = loc.size()
+        dist = (loc[:, :, None, :] - loc[:, None, :, :]).norm(p=2, dim=-1)
+
+        facility = torch.tensor([], dtype=torch.int64, device=loc.device)
+        cover_num = torch.zeros(batch_size, 1, dtype=torch.long, device=loc.device)
+        prev_a = torch.zeros(batch_size, 1, dtype=torch.long, device=loc.device)
+
+        return StateMCLP(
+            loc=loc,
+            p=p,
+            radius=radius,
+            dist=dist,
+            ids=torch.arange(batch_size, dtype=torch.int64, device=loc.device)[:, None],  # Add steps dimension
+            # Keep visited with depot so we can scatter efficiently (if there is an action for depot)
+            visited_=(  # Visited as mask is easier to understand, as long more memory efficient
+                torch.zeros(
+                    batch_size, 1, n_loc,
+                    dtype=torch.bool, device=loc.device
+                )
+                if visited_dtype == torch.bool
+                else torch.zeros(batch_size, 1, (n_loc + 63) // 64, dtype=torch.int64, device=loc.device)  # Ceil
+            ),
+            mask_cover=(  # Visited as mask is easier to understand, as long more memory efficient
+                torch.zeros(
+                    batch_size, 1, n_loc,
+                    dtype=torch.bool, device=loc.device
+                )
+                if visited_dtype == torch.bool
+                else torch.zeros(batch_size, 1, (n_loc + 63) // 64, dtype=torch.int64, device=loc.device)  # Ceil
+            ),
+            dynamic=torch.ones(batch_size, 1, n_loc, dtype=torch.float, device=loc.device),
+            facility=facility,
+            cover_num=cover_num,
+            prev_a=prev_a,
+            first_a=prev_a,
+            cur_coord=None,
+            i=torch.zeros(1, dtype=torch.int64, device=loc.device)  # Vector with length num_steps
+        )
+
+    def get_final_cost(self):
+
+        assert self.all_finished()
+
+        return self.cover_num
+
+    def get_cover_num(self, facility):
+        """
+        :param facility: list, a list of facility index list,  if None, generate randomly
+        :return: obj val of given facility_list
+        """
+
+        batch_size, n_loc, _ = self.loc.size()
+        _, p = facility.size()
+        radius = self.radius
+        facility_tensor = facility.unsqueeze(-1).expand_as(torch.Tensor(batch_size, p, n_loc))
+        f_u_dist_tensor = self.dist.gather(1, facility_tensor)
+        mask = f_u_dist_tensor < radius
+        mask = torch.sum(mask, dim=1)
+        cover_num = torch.count_nonzero(mask, dim=-1)
+
+        return cover_num
+
+    def update(self, selected):
+
+        # Update the state
+        cur_selected = selected.unsqueeze(-1)
+        prev_a = cur_selected  # Add dimension for step
+        cur_coord = self.loc[self.ids, prev_a]
+
+        cur_facility = self.facility
+        new_facility = torch.cat((cur_facility, cur_selected), dim=1)
+        new_cover_num = self.get_cover_num(new_facility)
+
+        first_a = prev_a if self.i.item() == 0 else self.first_a
+
+        if self.visited_.dtype == torch.bool:
+            # Add one dimension since we write a single value
+            visited_ = self.visited_.scatter(-1, prev_a[:, :, None], 1)
+        else:
+            visited_ = mask_long_scatter(self.visited_, prev_a)
+
+        # mask covered cities
+        batch_size, sequence_size, _ = self.loc.size()
+        dists = (self.loc[self.ids.squeeze(-1)] - cur_coord).norm(p=2, dim=-1).unsqueeze(1)
+        dynamic = self.dynamic.clone()
+        mask_cover = dists <= self.radius
+
+        mask = dists > self.radius
+        dynamic_update = dists.masked_fill(mask, value=1)
+        dynamic = dynamic.mul(dynamic_update)
+
+        return self._replace(first_a=first_a, prev_a=prev_a, visited_=visited_, mask_cover=mask_cover,
+                             dynamic=dynamic, cover_num=new_cover_num, cur_coord=cur_coord, i=self.i + 1)
+
+    def all_finished(self):
+        # Exactly n steps
+        return self.i == self.p
+
+    def get_current_node(self):
+        return self.prev_a
+
+    def get_mask(self):
+        return self.visited_
+
+    def get_dynamic(self):
+        return self.dynamic
